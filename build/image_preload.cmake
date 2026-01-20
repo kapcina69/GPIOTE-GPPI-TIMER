65c2b62b7503d91baf42868cb37bf0026b98135d
kapcina69 <lukasmolovicpp@gmail.com>
2026-01-20 15:36:29 +0100

    Add .gitignore and remove build directory from tracking

--- build/image_preload.cmake
+++
@@ -1,8 +0,0 @@
-# Generated file that can be used to preload variant images
-set(BOARD "nrf52840dk/nrf52840" CACHE INTERNAL "NCS child image controlled")
-set(CLI_CONFIG_DEBUG_THREAD_INFO "n" CACHE INTERNAL "NCS child image controlled")
-set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
-set(DEBUG_THREAD_INFO "On" CACHE INTERNAL "NCS child image controlled")
-set(DTC_OVERLAY_FILE "C:/ncs/v2.9.1/modules/hal/nordic/nrfx/samples/src/nrfx_gppi/boards/nrf52840dk_nrf52840.overlay" CACHE INTERNAL "NCS child image controlled")
-set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
-set(WEST_PYTHON "C:/ncs/toolchains/0b393f9e1b/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
